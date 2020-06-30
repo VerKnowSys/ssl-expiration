9f1c151097db79903e745ea9fcce2b7998947ca4
Daniel (dmilith) Dettlaff <daniel@centra.com>
2020-06-30 14:02:47 +0200

    new: Cleaned up library code. Implemented fixes by doggor and wojtekw92 from original crate. More in details.
    * Applied fix from: https://github.com/onur/ssl-expiration/pull/4 by wojtekw92
    * Applied fix from: https://github.com/onur/ssl-expiration/issues/5 by doggor
    * Applied fix for: https://github.com/onur/ssl-expiration/issues/6 by me
    * Fixed build issues with not existing .as_ptr() for native calls to ASN1_TIME_diff()
    * Extended tests a bit with check of days() returning wanted value
    * Cleaned and reformatted old code with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -13,53 +13,52 @@
 //! }
 //! ```
 
-extern crate foreign_types_shared;
-extern crate openssl;
-extern crate openssl_sys;
 #[macro_use]
 extern crate error_chain;
 
-use std::os::raw::c_int;
-use std::net::{TcpStream, ToSocketAddrs};
-use std::error::Error;
-use openssl::ssl::{Ssl, SslContext, SslMethod, SslVerifyMode};
-use openssl::asn1::Asn1Time;
-use openssl_sys::ASN1_TIME;
-use foreign_types_shared::{ForeignType,ForeignTypeRef};
 use error::Result;
+use openssl::asn1::*;
+use openssl::ssl::{Ssl, SslContext, SslMethod, SslVerifyMode};
+use openssl_sys::ASN1_TIME;
+use std::net::{TcpStream, ToSocketAddrs};
+use std::os::raw::c_int;
 
 
 extern "C" {
-    fn ASN1_TIME_diff(pday: *mut c_int,
-                      psec: *mut c_int,
-                      from: *const ASN1_TIME,
-                      to: *const ASN1_TIME);
+    fn ASN1_TIME_diff(
+        pday: *mut c_int,
+        psec: *mut c_int,
+        from: *const ASN1_TIME,
+        to: *const ASN1_TIME,
+    );
 }
 
 
 pub struct SslExpiration(c_int);
-
 
 impl SslExpiration {
     /// Creates new SslExpiration from domain name.
     ///
     /// This function will use HTTPS port (443) to check SSL certificate.
     pub fn from_domain_name(domain: &str) -> Result<SslExpiration> {
-        SslExpiration::from_addr(format!("{}:443", domain))
+        SslExpiration::from_addr(format!("{}:443", domain), domain)
     }
 
     /// Creates new SslExpiration from SocketAddr.
-    pub fn from_addr<A: ToSocketAddrs>(addr: A) -> Result<SslExpiration> {
+    pub fn from_addr<A: ToSocketAddrs>(addr: A, domain: &str) -> Result<SslExpiration> {
         let context = {
             let mut context = SslContext::builder(SslMethod::tls())?;
             context.set_verify(SslVerifyMode::empty());
             context.build()
         };
-        let connector = Ssl::new(&context)?;
+        let mut connector = Ssl::new(&context)?;
+        connector.set_hostname(domain)?;
         let stream = TcpStream::connect(addr)?;
-        let stream = connector.connect(stream)
-            .map_err(|e| error::ErrorKind::HandshakeError(e.description().to_owned()))?;
-        let cert = stream.ssl()
+        let stream = connector
+            .connect(stream)
+            .map_err(|e| error::ErrorKind::HandshakeError(e.to_string()))?;
+        let cert = stream
+            .ssl()
             .peer_certificate()
             .ok_or("Certificate not found")?;
 
@@ -69,13 +68,12 @@ impl SslExpiration {
         unsafe {
             let ptr_pday: *mut c_int = &mut pday;
             let ptr_psec: *mut c_int = &mut psec;
-            ASN1_TIME_diff(ptr_pday,
-                           ptr_psec,
-                           now.as_ptr(),
-                           cert.not_after().as_ptr());
+            let now_ptr = &now as *const _ as *const _;
+            let after_ptr = &cert.not_after() as *const _ as *const _;
+            ASN1_TIME_diff(ptr_pday, ptr_psec, *now_ptr, *after_ptr);
         }
 
-        Ok(SslExpiration(pday * 24 * 60 * 60 - psec))
+        Ok(SslExpiration(pday * 24 * 60 * 60 + psec))
     }
 
     /// How many seconds until SSL certificate expires.
@@ -98,11 +96,8 @@ impl SslExpiration {
     }
 }
 
-
-
 pub mod error {
     use std::io;
-    use openssl;
 
     error_chain! {
         foreign_links {
@@ -121,9 +116,28 @@ pub mod error {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+
     #[test]
-    fn test_ssl_expiration() {
-        assert!(!SslExpiration::from_domain_name("google.com").unwrap().is_expired());
-        assert!(SslExpiration::from_domain_name("expired.identrustssl.com").unwrap().is_expired());
+    fn test_ssl_not_expired() {
+        assert!(
+            !SslExpiration::from_domain_name("google.com")
+                .unwrap()
+                .is_expired()
+        );
+        let days = SslExpiration::from_domain_name("google.com")
+            .unwrap()
+            .days();
+        assert!(days > 14)
+    }
+
+
+    #[test]
+    fn test_ssl_expired() {
+        assert!(
+            SslExpiration::from_domain_name("expired.identrustssl.com")
+                .unwrap()
+                .is_expired()
+        );
     }
 }
