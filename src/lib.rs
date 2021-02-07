622c90490376dbdfeb5ce857ac4420efc3585252
Daniel (dmilith) Dettlaff <daniel@centra.com>
2021-02-07 10:49:06 +0100

    imp: Added one more test for unresolvable domain. Cleanup of imports. Docs cleanup.

--- src/lib.rs
+++ src/lib.rs
@@ -29,12 +29,18 @@
 #[macro_use]
 extern crate error_chain;
 
-use error::Result;
-use openssl::asn1::*;
-use openssl::ssl::{Ssl, SslContext, SslMethod, SslVerifyMode};
+use error::{Error, ErrorKind, Result};
+use error_chain::State;
+use openssl::{
+    asn1::*,
+    ssl::{Ssl, SslContext, SslMethod, SslVerifyMode},
+};
 use openssl_sys::ASN1_TIME;
-use std::net::{TcpStream, ToSocketAddrs};
-use std::os::raw::c_int;
+use std::{
+    net::{TcpStream, ToSocketAddrs},
+    os::raw::c_int,
+    time::Duration,
+};
 
 
 extern "C" {
@@ -52,11 +58,12 @@ pub struct SslExpiration(c_int);
 impl SslExpiration {
     /// Creates new SslExpiration from domain name.
     ///
-    /// This function will use HTTPS port (443) to check SSL certificate.
+    /// This function will use HTTPS port (443) to check SSL certificate with 30 seconds timeout.
     pub fn from_domain_name(domain: &str) -> Result<SslExpiration> {
         SslExpiration::from_addr(format!("{}:443", domain), domain, 30) // seconds
     }
 
+    /// This function will use HTTPS port (443) to check SSL certificate with custom timeout.
     pub fn from_domain_name_with_timeout(domain: &str, timeout: u64) -> Result<SslExpiration> {
         SslExpiration::from_addr(format!("{}:443", domain), domain, timeout)
     }
@@ -178,8 +185,17 @@ mod tests {
 
 
     #[test]
+    fn test_unresolvable_timeout_chain() {
+        SslExpiration::from_domain_name_with_timeout("unresolvable.net", 3)
+            .and_then(|_| Ok(assert!(false)))
+            .map_err(|e| println!("Error: {:?}", e))
+            .unwrap_or_else(|_| assert!(true));
+    }
+
+
+    #[test]
     fn test_sufficient_timeout_chain() {
-        SslExpiration::from_domain_name_with_timeout("google.com", 300)
+        SslExpiration::from_domain_name_with_timeout("google.com", 30)
             .and_then(|_| Ok(assert!(true)))
             .unwrap_or_else(|_| assert!(false));
     }
